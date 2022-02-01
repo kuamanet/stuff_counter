4b253533ac1027e6aa4efe6a2e3bab8d3b38331c
Daniele De Matteo <daniele@kuama.net>
2022-02-01 14:22:33 +0100

    add mocktail dependency add tests for already written actions

---
+++ test/counters/test_utils.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:stuff_counter/counters/core/counters_repository.dart';
+
+class CounterRepositoryMock extends Mock implements CountersRepository {}
