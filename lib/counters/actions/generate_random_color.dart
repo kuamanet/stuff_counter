610a9a84d0147690dce6d119fb681c65c313b05f
Daniele De Matteo <daniele@kuama.net>
2022-02-09 14:19:32 +0100

    better random color generator

--- lib/counters/actions/generate_random_color.dart
+++ lib/counters/actions/generate_random_color.dart
@@ -8,6 +8,6 @@ import 'package:kcounter/counters/core/action.dart' as counters;
 class GenerateRandomColor extends counters.Action<Color> {
   @override
   Future<Color> run() async {
-    return Colors.primaries[Random.secure().nextInt(Colors.primaries.length)];
+    return Color((Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
   }
 }
