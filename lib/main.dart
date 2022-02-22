8270140dea236a781e15c7c72a2ac761bc76b5d2
Daniele <daniele@kuama.net>
2022-02-22 23:48:29 +0100

    remove web platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:kcounter/counters_app.dart';
-import 'package:kcounter/landing_app.dart';
 
 void main() async {
-  if (kIsWeb) {
-    launchLandingApp();
-  } else {
-    await launchCountersApp();
-  }
+  await launchCountersApp();
 }
--- lib/navigation/web_route.dart
+++
@@ -1,3 +0,0 @@
-const String landingPath = "/";
-const String privacyPath = "/privacy-policy";
-const String termsAndConditionsPath = "/terms-and-conditions";
